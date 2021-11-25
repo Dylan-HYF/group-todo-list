99eb9484dc7f52db2e8e598877446a8c004c994c
sernoskie <sernoski@sheridancollege.ca>
2021-11-25 13:56:02 -0500

    minor details added and style logo

--- login.php
+++ login.php
@@ -5,13 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Log In</title>
 
-        <link rel="stylesheet" href="initialize.css">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="css/initialize.css">
+        <link rel="stylesheet" href="css/main.css">
        
     </head>
     <body>
 
         <fieldset>
+            <img src="assets/palendar-logo.png" style="width:200px"/> <br>
             <form action="process-login.php" method="POST">
             <label> Username </label><br>
             <input type="text" name="username" /><br>
@@ -19,6 +20,7 @@
             <input type="password" name="password" /><br>
             <input type="submit" value="Log In"/>   
             </form>
+            <p>Don't have an account? <a href="sign-up.php">Sign up</a></p>
         </fieldset>
     
     </body>
--- sign-up.php
+++ sign-up.php
@@ -5,22 +5,30 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Sign Up</title>
 
-        <link rel="stylesheet" href="initialize.css">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="css/initialize.css">
+        <link rel="stylesheet" href="css/main.css">
         
     </head>
     <body>
 
         <fieldset>
+            <p>Welcome to <br>
+            <img src="assets/palendar-logo.png" style="width:200px"/> <br>
+            Sign up below to get started!</p>
             <form method="POST" action="process-sign-up.php">
                 <label>Username</label><br>
                 <input type="text" name="username" required><br>
                 <label>Password</label><br>
                 <input type="text" name="password" required><br>
-                <label>Profile Color</label><br>
-                <input type="color" name="color" required><br>
+                <label>Pick your profile colour</label><br>
+                <input type="color" name="color" value="#d1cee7" required><br>
+                <label>Join a Group?</label>
+                <input type="checkbox" name="groupy"><br>
+                <!--if box is checked, search bar shows up-->
+                <input type="search" name="q" ><br>
                 <input type="submit" value="Sign Up">
             </form>
+            <p>Already have an account? <a href="login.php">Log in</a></p>
         </fieldset>
 
     </body>
