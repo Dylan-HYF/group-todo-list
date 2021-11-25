73bd8b78b741ece8ba43f3ddb79c74183980d0c9
sainold00 <tsuireynold@hotmail.com>
2021-11-25 10:40:20 -0500

    fml2

--- userProfile.php
+++ userProfile.php
@@ -7,8 +7,28 @@
     <title>User Profile</title>
 </head>
 <body>
-    <a>User Name:</a><hr>
-    <a>
+    <nav>
+        <a href = "index.php"><img class="logo" src="logo.png" alt="Palender">
+        <a href="dashboard.php">Dashboard</a>
+        <a href="group.php">Groups</a>
+
+        <a href="logout.php">Logout</a>
+        <a href = "userProfile.php"><img src="user.png" alt="user"></a>
+
+    </nav>
+    <h1>Username</h1><hr>
+    <h2>Profile Icon</h2>
+    <label for="color">Color</label><br>
+    <input type="color" value="#D0CEE7" id="color"><hr>
+    <br>
+    <a>Group List</a><br>
+    <ul>
+        <li>Group name
+        <li>Group name
+        <li>Group name
+    </ul><br>
+    <br>
+    <a href="editProfile.php">Edit Profile</a>
 
 </body>
 </html>
