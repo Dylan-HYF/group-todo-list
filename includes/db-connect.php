f14ed67721a5a4e04608af1c7e02b526182505ad
Dylan-HYF <68765633+Dylan-HYF@users.noreply.github.com>
2021-11-25 19:49:05 -0500

    finalize first presentation

--- includes/db-connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dsn = "mysql:host=142.55.32.48;dbname=huangyou_group_todo_list;charset=utf8mb4";
-$dbusername = 'huangyou_hyf';
-$dbpassword = '!]GDM?)joe]J';
-
-$pdo = new PDO($dsn, $dbusername, $dbpassword);
--- userProfile.php
+++ userProfile.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/initialize.css">
+    <link rel="stylesheet" href="css/nav.css">
     <title>User Profile</title>
 </head>
+
 <body>
-    <nav>
+    <!-- <nav>
         <a href = "index.php"><img class="logo" src="logo.png" alt="Palender">
         <a href="dashboard.php">Dashboard</a>
         <a href="group.php">Groups</a>
@@ -15,20 +19,40 @@
         <a href="logout.php">Logout</a>
         <a href = "userProfile.php"><img src="user.png" alt="user"></a>
 
-    </nav>
-    <h1>Username</h1><hr>
-    <h2>Profile Icon</h2>
-    <label for="color">Color</label><br>
-    <input type="color" value="#D0CEE7" id="color"><hr>
-    <br>
-    <a>Group List</a><br>
-    <ul>
-        <li>Group name
-        <li>Group name
-        <li>Group name
-    </ul><br>
-    <br>
-    <a href="editProfile.php">Edit Profile</a>
+    </nav> -->
+    <header>
+        <nav>
+            <img src="images/logo.png" alt="logo" id="logo">
+            <ul>
+                <li><a href="index.html">Dashboard</a></li>
+                <li><a href="group.html">Groups</a></li>
+            </ul>
+            <div class="username">
+                <span>Hi username!</span>
+                <a href="userProfile.php">
+                    <div class="avatar">R</div>
+                </a>
+            </div>
+        </nav>
+    </header>
+    <section style="padding: 20px">
+        <h1>Username</h1>
+        <hr>
+        <h2>Profile Icon</h2>
+        <label for="color">Color</label><br>
+        <input type="color" value="#D0CEE7" id="color">
+        <hr>
+        <br>
+        <a>Group List</a><br>
+        <ul>
+            <li>Group name
+            <li>Group name
+            <li>Group name
+        </ul><br>
+        <br>
+        <a href="#">Edit Profile</a>
 
+    </section>
 </body>
+
 </html>
