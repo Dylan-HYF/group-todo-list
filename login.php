8c940c9b91233252ba2a65a2a698110c0c74b720
sernoskie <sernoski@sheridancollege.ca>
2021-12-05 14:25:57 -0500

    CSS updated for log in and signup pages

--- login.php
+++ login.php
@@ -7,37 +7,38 @@
     <title>Log In</title>
 
     <link rel="stylesheet" href="css/initialize.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/login-signup.css">
     <!-- <link rel="stylesheet" href="css/nav.css"> -->
 
 </head>
 
 <body>
-    <!--nav bar-->
+    <main class="bg">
+        <!--nav bar-->
 
-    <!-- <header>
-        <nav>
-            <img src="images/logo.png" alt="logo" id="logo">
-            <ul>
-                <li><a href="login.php">Log In</a></li>
-                <li><a href="sign-up.php">Sign Up</a></li>
-            </ul>
-        </nav>
-    </header> -->
+        <!-- <header>
+            <nav>
+                <img src="images/logo.png" alt="logo" id="logo">
+                <ul>
+                    <li><a href="login.php">Log In</a></li>
+                    <li><a href="sign-up.php">Sign Up</a></li>
+                </ul>
+            </nav>
+        </header> -->
 
-    <div id="login">
-        <!-- log in form -->
-        <img src="images/logo-colour.png" id="clogo" /> <br>
-        <form action="process-login.php" method="POST">
-            <label> Username </label><br>
-            <input type="text" name="username" /><br>
-            <label> Password </label><br>
-            <input type="password" name="password" /><br>
-            <input type="submit" value="Log In" />
-        </form>
-        <p>Don't have an account? <a href="sign-up.php" style="color: #9892C7">Sign up</a></p>
-    </div>
-
+        <div id="login">
+            <!-- log in form -->
+            <img src="images/logo-colour.png" id="clogo" /> <br>
+            <form action="process-login.php" method="POST">
+                <div style="text-align: left; transform: translate(70px, 0px);"><label> Username </label></div>
+                <input type="text" name="username" /><br>
+                <div style="text-align: left; transform: translate(70px, 0px);"><label> Password </label></div>
+                <input type="password" name="password" /><br>
+                <input type="submit" value="Log In" />
+            </form>
+            <p>Don't have an account? <a href="sign-up.php">Sign up</a></p>
+        </div>
+    </main>
 </body>
 
 </html>
--- sign-up.php
+++ sign-up.php
@@ -7,51 +7,53 @@
     <title>Sign Up</title>
 
     <link rel="stylesheet" href="css/initialize.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/login-signup.css">
     <!-- <link rel="stylesheet" href="css/nav.css"> -->
 
 </head>
 
 <body>
-    <!-- nav bar-->
-    <!-- <header>
-        
-        <nav>
-            <img src="images/logo.png" alt="logo" id="logo">
-            <ul>
-                <li><a href="login.php">Log In</a></li>
-                <li><a href="sign-up.php">Sign Up</a></li>
-            </ul>
-        </nav>
-    </header> -->
+    <main class="bg">
+        <!-- nav bar-->
+        <!-- <header>
+            
+            <nav>
+                <img src="images/logo.png" alt="logo" id="logo">
+                <ul>
+                    <li><a href="login.php">Log In</a></li>
+                    <li><a href="sign-up.php">Sign Up</a></li>
+                </ul>
+            </nav>
+        </header> -->
 
-    <div id="signup">
-        <!--sign up form-->
-        <p>Welcome to <br>
-            <img src="images/logo-colour.png" id="clogo" /> <br>
-            Sign up below to get started!
-        </p>
-        <form method="POST" action="process-sign-up.php" name="signUp">
-            <label>Username</label><br>
-            <input type="text" name="username" required><br>
-            <label>Password</label><br>
-            <input type="text" name="password" required><br>
-            <label>Pick your profile colour</label><br>
-            <input type="color" name="color" value="#d1cee7" required><br>
-            <!-- <label>Join a Group</label>
-            <input type="radio" name="groupSet" value="1" required><br>
-            <label>Create a Group</label>
-            <input type="radio" name="groupSet" value="2">
-            <input type="text" placeholder="group name" name="groupName" style="display:none">
-            <br>
-            <label>Not now</label>
-            <input type="radio" name="groupSet" value="3"><br> -->
-            <!--if box is checked, search bar shows up-->
-            <!-- <input type="search" name="q"><br> -->
-            <input type="submit" value="Sign Up">
-        </form>
-        <p>Already have an account? <a href="login.php" style="color: #9892C7">Log in</a></p>
-    </div>
+        <div id="signup">
+            <!--sign up form-->
+            <p>Welcome to<br>
+                <img src="images/logo-colour.png" id="clogo" /> <br>
+                <span style="font-weight: 300;">Sign up below to get started!</span>
+            </p>
+            <form method="POST" action="process-sign-up.php" name="signUp">
+                <div style="text-align: left; transform: translate(70px, 0px);"><label>Username</label></div>
+                <input type="text" name="username" required><br>
+                <div style="text-align: left; transform: translate(70px, 0px);"><label>Password</label></div>
+                <input type="text" name="password" required><br>
+                <div style="text-align: left; transform: translate(70px, 0px);"><label>Pick your profile colour</label></div>
+                <input type="color" name="color" value="#d1cee7" required><br>
+                <!-- <label>Join a Group</label>
+                <input type="radio" name="groupSet" value="1" required><br>
+                <label>Create a Group</label>
+                <input type="radio" name="groupSet" value="2">
+                <input type="text" placeholder="group name" name="groupName" style="display:none">
+                <br>
+                <label>Not now</label>
+                <input type="radio" name="groupSet" value="3"><br> -->
+                <!--if box is checked, search bar shows up-->
+                <!-- <input type="search" name="q"><br> -->
+                <input type="submit" value="Sign Up">
+            </form>
+            <p>Already have an account? <a href="login.php">Log in</a></p>
+        </div>
+    </main>
     <!-- <script src="js/signUp.js"></script> -->
 </body>
 
